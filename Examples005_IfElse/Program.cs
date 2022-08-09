e895094e1e01b7f7aad37a8dcc9f25b62d9cd321
Maria Spiridonova <traffic2007@mail.ru>
2022-08-09 14:37:18 +0300

    If Else

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Манюшка");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
